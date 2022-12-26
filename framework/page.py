c6067284cec2d77da1c0a94937cab2c8a32938cc
Vadym Shkarbul <vadim.shkarbul@gmail.com>
2022-12-26 15:49:14 +0200

    Fixed page.py

--- framework/page.py
+++ framework/page.py
@@ -1,18 +1,34 @@
-from appium.webdriver.common.appiumby import AppiumBy
+from appium.webdriver import WebElement
+from selenium.common import NoSuchElementException
 
 
 class Page:
     def __init__(self, driver):
         self.driver = driver
 
-    def find_element(self, locator: str):
-        return self.driver.find_element(AppiumBy.ID, locator)
+    def check_element(self, locator: tuple) -> bool:
+        try:
+            self.driver.find_element(*locator)
+        except NoSuchElementException:
+            return False
+        else:
+            return True
 
-    def click_element(self, locator: str):
-        return self.driver.find_element(AppiumBy.ID, locator).click()
+    def find_element(self, locator: tuple) -> WebElement:
+        if not self.check_element(locator):
+            raise NoSuchElementException(f"Element with ID:{locator[1]} does not exist")
+        return self.driver.find_element(*locator)
 
-    def fill_field(self, locator: str, keys: str):
+    def click_element(self, locator: tuple) -> None:
+        element = self.find_element(locator)
+        element.click()
+
+    def fill_text_field(self, locator: tuple, keys: str) -> None:
         field = self.find_element(locator)
-        field.click()
         field.send_keys(keys)
-        return field
+
+    def wait(self, seconds: int) -> None:
+        self.driver.implicitly_wait(seconds)
+
+    def go_back(self) -> None:
+        self.driver.back()
